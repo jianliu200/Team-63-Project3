a810c60ed93dc0908529d87d99b8b4303af19b8e
Anna <44736708+annahuangg@users.noreply.github.com>
2022-12-05 23:28:32 -0600

    add popup checks

---
+++ excessreport.js
@@ -0,0 +1,157 @@
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
+import {Link} from 'react-router-dom';
+import './excessreport.css';
+
+function ExcessReport(){
+const [startdate, setStartDate] = useState('2022-10-02')
+const [enddate, setEndDate] = useState('2022-10-03')
+const[columns, setColumns] = useState([])
+const[starts, setStarts] = useState([])
+const[ends, setEnds] = useState([])
+const[excess,setExcess] = useState([])
+
+useEffect(() => {
+    const getColumns = async()=>{
+        await axios.get('http://localhost:6969/column').then((result)=>{
+            // console.log("It gets all of the columns")
+            setColumns(result.data)
+        })
+    }
+    getColumns()
+  },[]);
+//   console.log("This is all of the columns in the system: ", columns)
+  const change1 = event =>{
+    setStartDate(event.target.value)
+    console.log(startdate)
+  }
+
+  const change2 = event =>{
+    setEndDate(event.target.value)
+    console.log(enddate)
+  }
+
+
+  const callApiStart = async (startdate) =>{
+    //Object.values(temp1[0])[0]
+    await axios.get(`http://localhost:6969/supplydatestart/${startdate}`).then((result) => {
+        const new_list = []
+        //console.log("This is the data from the api start: ",result.data);
+        for(var i = 1; i<Object.values(result.data[0]).length; ++i){
+          //console.log("This is the value for each one for start: ",Object.values(result.data[0])[i])
+          new_list.push(Object.values(result.data[0])[i])
+        }
+        console.log("The new list for start is :",new_list)
+        setStarts(new_list)
+        //console.log("This is the starts array: ",starts)
+    });
+  
+  }
+    
+
+
+const callApiEnd = async (enddate) =>{ 
+    await axios.get(`http://localhost:6969/supplydateend/${enddate}`).then((result) => {
+      
+      const new_list = []
+        //console.log("This is the data from the api end: ",result.data);
+        for(var i = 1; i<Object.values(result.data[0]).length; ++i){
+          //console.log("This is the value for each one for end: ",Object.values(result.data[0])[i])
+          new_list.push(Object.values(result.data[0])[i])
+        }
+        console.log("The new list for end is :",new_list)
+        setEnds(new_list)
+        // console.log("This is the ends array: ",ends)
+      
+    });
+}
+
+const isExcess = async()=>{
+   console.log("This is starts: ",starts)
+    console.log("This is ends: ",ends)
+    console.log(columns);
+    const new_list = []
+    for(var i =1; i < columns.length;++i){
+        // console.log(columns[i])
+        // console.log("This is the start value", starts[i-1])
+        // console.log("This is the end value: ",ends[i-1])
+        const math = (starts[i-1]-ends[i-1])/(starts[i-1])
+        console.log("The math is ",math)
+        if(math < .1){
+          console.log("Made it bitch")
+          new_list.push(columns[i])
+        }
+        
+    }
+    console.log("This is the new list: ",new_list)
+    setExcess(new_list)
+}
+
+const displayTable = () => {
+  
+      return(
+       
+        <><table id="excessTable">
+            {/* <thread> */}
+              <tr>
+                <th>Food ID</th>
+              </tr>
+            {/* </thread> */}
+            <tbody>
+              {excess.map((item) => (
+                <tr>
+                  <td key = {item.column_name}>{item.column_name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table></>
+      )
+    
+  }
+
+  function finalResult(){
+    console.log("This is the start date: ",startdate);
+    console.log("This is the enddate: ",enddate);  
+    
+    callApiStart(startdate);
+    callApiEnd(enddate);
+    isExcess();
+    alert("Submitted!")
+    // console.log("This is the array for start: ",starts);
+    // console.log("This is the array for the end: ", ends);
+    displayTable();
+  }
+  
+
+return(
+    <><body>
+        
+        {/* <ul>
+            <button><Link to="/manager">Ordering System</Link></button>
+            <button><Link to="/manager/salesreport">Sales Report</Link></button>
+        </ul> */}
+    </body>
+    
+    
+    <h1 class="intro">Excess Report</h1>
+    
+      <div class="dateHeader">
+        <h3 class="start">Start</h3>
+        <h3 class="end">End</h3>
+      </div>
+      <div>
+      <input type="date" onChange={change1} value = {startdate} min="2022-10-02" max="2022-12-31" class="startDate"/>
+      <input type="date" onChange={change2} value = {enddate} min="2022-10-02" max="2022-12-31" class="endDate"/>
+      <button onClick = {()=> finalResult()} class="reddy">Submit</button>
+      </div>
+    
+    
+    {displayTable()}
+    </>
+
+)
+
+
+}
+
+export default ExcessReport
---
+++ manager.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import './App.css';
+import Receipt from './receipt';
+import {Link} from 'react-router-dom';
+import {Route,Routes} from 'react-router-dom';
+import Salesreport from './manager/salesreport';
+import ExcessReport from './manager/excessreport';
+import FoodUpdate from './manager/foodupdate';
+import { useEffect } from 'react'
+import { GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+import {useState} from 'react';
+
+
+
+//import LogIn from './OAuth/oafront.js'
+
+function Manager() {
+  const [ profile, setProfile ] = useState([]);
+  const clientId = '7130970063-8l4ukqnaa0o24aiklhbbb8vbo8rpos8a.apps.googleusercontent.com';
+  useEffect(() => {
+    const initClient = () => {
+      gapi.auth2.init({
+          clientId: clientId,
+          scope: ''
+      });
+    };
+    // gapi.load('client:auth2', initClient);
+    // var addScript = document.createElement('script');
+    // addScript.setAttribute('src', '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');
+    // document.body.appendChild(addScript);
+    // window.googleTranslateElementInit = googleTranslateElementInit;
+  }, [])
+    const logOut = () => {
+        setProfile(null);
+        document.getElementById("loginControl").hidden=false;
+        document.getElementById("btnManager").hidden=true;
+    
+        document.getElementById("btnGoogleLogin").hidden=false;
+        document.getElementById("btnGoogleLogout").hidden=true;
+        
+        document.getElementById("btnServer").hidden=true;
+        document.getElementById("btnCustomer").hidden=true;
+      };
+  return (
+    <>
+      <body>
+          <nav class="navigation">
+            <a href="/"><img src="/cfa-logo.png" width="50" height="50"></img></a>
+            <div class="more-header">
+              <a href="/App">Home</a>
+              <a href="/manager/salesreport">Sales Report</a>
+              <a href="/manager/excessreport">Excess Report</a>
+              <a href="/manager/foodUpdate">Food Update</a>
+              <GoogleLogout clientId={clientId} 
+                buttonText="Log out" onLogoutSuccess={logOut}
+              />
+            </div>
+          </nav>
+          <Routes>
+          <Route exact path = "/manager" element = {<Manager/>}/>
+            <Route exact path = "/manager/salesreport" element = {<Salesreport/>}/>
+            <Route exact path = "/manager/excessreport" element={<ExcessReport/>}/>
+            <Route exact path = "/manager/foodUpdate" element={<FoodUpdate/>}/>
+          </Routes>
+          <h1 class="intro">Manager</h1>
+          <Receipt></Receipt>
+      </body>
+    </>
+  );
+}
+
+export default Manager;
