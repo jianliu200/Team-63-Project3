491d78d4a539cafe0069920f034d1ecd14cc6a8f
jianliu200 <jiangyuanliu2002@gmail.com>
2022-12-06 15:39:40 -0600

    did not work

--- project3/src/database.js
+++ project3/src/database.js
@@ -205,12 +205,12 @@ app.post("/newsupply",async(req,res)=>{
 });
 
 //This is going to be in the serve react build folder from express
-app.use(express.static(path.join(path.dirname(path.basename(__dirname)), 'build')));
-console.log(path.dirname(path.basename(__dirname)));
+// app.use(express.static(path.join(path.dirname(path.basename(__dirname)), 'build')));
+// console.log(path.dirname(path.basename(__dirname)));
 
-app.get("*",async(req,res)=>{
-    res.sendFile(path.join(path.dirname(path.basename(__dirname)), 'build', 'index.html'));
-});
+// app.get("*",async(req,res)=>{
+//     res.sendFile(path.join(path.dirname(path.basename(__dirname)), 'build', 'index.html'));
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
